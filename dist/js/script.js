e7d134bb5c8364571ab95a2e29c645e912d528ba
TkTnX <timur20-09@mail.ru>
2024-09-21 18:01:10 +0500

    мой первый проект на Pug - готов

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,24 @@
+// BURGER MODAL
+
+const burgerModalBtn = document.querySelector("#headerBurgerBtn");
+const headerBurgerClose = document.querySelector("#headerBurgerClose");
+const burgerModal = document.querySelector("#headerBurger");
+
+burgerModalBtn.addEventListener("click", () => {
+  burgerModal.classList.toggle("active");
+  document.body.style.overflow = "hidden";
+});
+
+
+headerBurgerClose.addEventListener("click", () => {
+  burgerModal.classList.remove("active");
+  document.body.style.overflow = "visible";
+
+
+})
+
+// SWIPER
+
 const swiper = new Swiper(".swiper", {
   navigation: {
     nextEl: ".swiper-button-next",
@@ -6,8 +27,21 @@ const swiper = new Swiper(".swiper", {
 
   scrollbar: {
     el: ".swiper-scrollbar",
+    draggable: true,
   },
 
-  slidesPerView: 4,
-  spaceBetween: 40,
+  slidesPerView: 1.3,
+  spaceBetween: 20,
+  breakpoints: {
+    450: {
+      slidesPerView: 2,
+    },
+    650: {
+      slidesPerView: 3,
+    },
+    900: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
 });
