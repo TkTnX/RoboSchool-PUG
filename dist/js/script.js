387caaf1be28f9155e27b87ea8d5292e59db5fd5
TkTnX <timur20-09@mail.ru>
2024-09-21 10:59:32 +0500

    вёрстка без адаптива готова

---
+++ dist/js/script.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper(".swiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+
+  slidesPerView: 4,
+  spaceBetween: 40,
+});
